fcb4e9a47d942846534f1067ca28c31bfa30caa8
Emily Gong <egong@terpmail.umd.edu>
2020-01-18 16:16:22 -0500

    added fourth user and transcript

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
-basedir = os.path.dirname(__file__)
-# basedir = os.path.abspath(os.path.dirname(__file__))
+import winter.users
+
+# basedir = os.path.dirname(__file__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+USER1 = winter.users.User(uid=1, username='Tyler', real_name='Pablo E.')
+USER2 = winter.users.User(uid=2, username='Charlie', real_name='Tyrone S.')
+
 class Config(object):
     SECRET_KEY = 'SECRET'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
